bf1b2dde7c2677a5b6b3719fda96991eeee22f1d
jsh <sehyun.dev@gmail.com>
2023-10-01 22:56:20 +0900

    feat: 게시물 생성하기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardServce: BoardsService) {}
 
   @Get('')
-  getAllBoards() {
+  getAllBoards(): Board[] {
     return this.boardServce.getAllBoards();
+  }
+
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardServce.createBoard(title, description);
   }
 }
