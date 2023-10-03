1964bc2cd8532e7ff9523ef4e1d0ad9560555fca
jsh <sehyun.dev@gmail.com>
2023-10-03 22:26:45 +0900

    feat: id로 특정 게시물 삭제하기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -12,7 +20,7 @@ export class BoardsController {
     return this.boardService.getAllBoards();
   }
 
-  @Get(':id')
+  @Get('/:id')
   getBoardById(@Param('id') id: string): Board {
     return this.boardService.getBoardById(id);
   }
@@ -20,5 +28,10 @@ export class BoardsController {
   @Post()
   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardService.createBoard(createBoardDto);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardService.deleteBoard(id);
   }
 }
