3f92582daea3719615310fb36f8eeffbb3d0d1e3
jsh <sehyun.dev@gmail.com>
2023-10-05 23:49:50 +0900

    feat: 커스텀파이프 추가

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,23 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(
+        `'${value}'는 게시글 상태값으로 사용할 수 없습니다.`,
+      );
+    }
+
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
+  }
+}
